eb5e5286d382718949ff407e4fd88cbad30eb28e
Luca Joos <1a85ra7z@gmail.com>
2022-04-16 14:54:54 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,63 @@
+import core from '@actions/core'
+import github from '@actions/github'
+import pr from './pr'
+import {Octokit} from '@octokit/rest'
+
+export type Options = {
+  token: string
+  type: string
+  owner: string
+  repo: string
+}
+
+async function run(): Promise<void> {
+  try {
+    const types = ['pr']
+    const token = core.getInput('token') || process.env.GITHUB_TOKEN || ''
+    const type = core.getInput('type') || ''
+    const owner = core.getInput('owner') || github.context.repo.owner || ''
+    const repo = core.getInput('repo') || github.context.repo.repo || ''
+
+    if (token ? token.length > 0 : true) {
+      core.setFailed(`Unspecified field 'token'`)
+      return
+    }
+
+    if (type ? !types.includes(type) : true) {
+      core.setFailed(`Unspecified or unknown 'type' provided`)
+      return
+    }
+
+    if (owner ? owner.length > 0 : true) {
+      core.setFailed(`Unspecified field 'owner'`)
+      return
+    }
+
+    if (repo ? repo.length > 0 : true) {
+      core.setFailed(`Unspecified field 'repo'`)
+      return
+    }
+
+    const options: Options = {token, type, owner, repo}
+
+    const octokit = new Octokit({
+      auth: `token ${options.token}`,
+      baseUrl: 'https://api.github.com'
+    })
+
+    if (options.type === 'pr') {
+      const ref = core.getInput('ref')
+
+      if (ref ? ref.length > 0 : true) {
+        core.setFailed(`Unspecified field 'ref' is required for type 'pr'`)
+        return
+      }
+
+      await pr(octokit, options, ref)
+    }
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
---
+++ src/pr.ts
@@ -0,0 +1,30 @@
+import {Octokit} from '@octokit/rest'
+import {Options} from './main'
+import core from '@actions/core'
+
+async function pr(
+  octokit: Octokit,
+  options: Options,
+  ref: string
+): Promise<void> {
+  return new Promise(async resolve => {
+    try {
+      const {data} = await octokit.pulls.get({
+        owner: options.owner,
+        repo: options.repo,
+        pull_number: parseInt(ref.split('refs/pull/')[1].split('/')[0])
+      })
+
+      core.notice(JSON.stringify(data))
+
+      resolve()
+    } catch (e: any) {
+      core.warning(
+        `Could not find PR ${options.owner}/${options.repo}#${pr}: ${e.message}`
+      )
+      return null
+    }
+  })
+}
+
+export default pr
