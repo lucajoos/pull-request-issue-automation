423d88167d7e771c1f0c1c1df318867cd8c4ad50
Luca Joos <1a85ra7z@gmail.com>
2022-04-16 15:46:32 +0200

    Check author regex

--- src/pr.ts
+++ src/pr.ts
@@ -7,24 +7,37 @@ async function pr(
   options: Options,
   ref: string
 ): Promise<void> {
-  return new Promise(async resolve => {
-    try {
-      const {data} = await octokit.pulls.get({
-        owner: options.owner,
-        repo: options.repo,
-        pull_number: parseInt(ref.split('refs/pull/')[1].split('/')[0])
-      })
+  try {
+    const {data} = await octokit.pulls.get({
+      owner: options.owner,
+      repo: options.repo,
+      pull_number: parseInt(ref.split('refs/pull/')[1].split('/')[0])
+    })
 
-      core.notice(JSON.stringify(data))
-
-      resolve()
-    } catch (e: any) {
-      core.warning(
-        `Could not find PR ${options.owner}/${options.repo}#${pr}: ${e.message}`
-      )
-      return null
+    if(typeof data !== 'object') {
+      core.setFailed(`Could not find PR ${options.owner}/${options.repo}#${pr}: Missing response data`)
+      return
     }
-  })
+
+    const author = core.getInput('author')
+
+    if(author ? author.length === 0 : true) {
+      core.setFailed(`Unspecified field 'author'`)
+      return
+    }
+
+    if(!new RegExp(author, 'g').test(data?.user?.login || '')) {
+      core.warning(`Ignore pull request because user is not in 'author' field`)
+    }
+
+    core.notice(JSON.stringify(data))
+  } catch (e: any) {
+    core.setFailed(
+      `Could not find PR ${options.owner}/${options.repo}#${pr}: ${e.message}`
+    )
+
+    return
+  }
 }
 
 export default pr
